17d2ccd68973195fa586ad5be64b0b64bded27fd
unknown <wuchaowu@NH03ITNB7000141.yihaodian.com>
2015-12-17 10:41:18 +0800

    Signed-off-by: unknown <wuchaowu@NH03ITNB7000141.yihaodian.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# chrome-plugins-qrcode
+## 功能:
+1. 默认根据当前页面路径的URL生成二维码
+2. 在输入框手动输入文本，点击按钮生成二维码
+
+## License 
+MIT License
---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+;!(function (){
+	var $input = document.getElementById("input");
+	var $btn = document.getElementById("btn");
+	var $about = document.getElementById("about");
+	var qrcode;
+
+	var options = {
+		text: "",
+		width: 160,
+		height: 160,
+		colorDark: "#000000",
+		colorLight: "#ffffff",
+		correctLevel: QRCode.CorrectLevel.H
+	}
+
+	chrome.tabs.getSelected(null, function(tab) {
+		options.text = tab.url;
+
+		$input.value = options.text;
+
+		qrcode = new QRCode("qrcode", options);
+	});
+	document.onkeydown=function(){
+	 if(event.keyCode==13)   
+	 {   
+		qrcode.clear();
+		qrcode.makeCode($input.value);
+	 } 
+	}
+	$btn.onclick = function(){
+		qrcode.clear();
+		qrcode.makeCode($input.value);
+	}
+	$about.onclick=function (){
+		window.open("https://github.com/wchaowu/qrcode_chrome_plugs");
+	}	
+})();
+
+
